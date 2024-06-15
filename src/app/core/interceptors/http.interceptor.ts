d541b2978248ba7279e4dea27d557acf9415c3fa
Gokul <gokulshyam567@gmail.com>
2024-06-15 00:16:04 -0400

    fix: table, tailwind forms css

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 
 @Injectable()
-export class OfssInterceptor implements HttpInterceptor {
+export class LpInterceptor implements HttpInterceptor {
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // return next.handle(httpRequest);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,59 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  /*corePlugins: {
+    container: false,
+  },*/
+  future: {},
+  content: [
+    './src/**/*.html', './src/**/*.ts'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+        'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
+      },
+      gridTemplateRows: {
+        'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+        'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
+      },
+    },
+    /*container: (theme) => ({
+      center: true,
+      padding: theme('spacing.2'),
+    }),*/
+    /*container: {},*/
   },
-  plugins: [],
-}
-
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
+    ({
+      addComponents,
+      theme,
+    }) => {
+      addComponents({
+        '.container': {
+          marginInline: 'auto',
+          paddingInline: theme('spacing.2'),
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1580px',
+          },
+        },
+      });
+    }],
+  important: true,
+};
