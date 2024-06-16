d30534653d6783251daea2ff13eee2e30c0b4748
Gokul <gokulshyam567@gmail.com>
2024-06-16 00:22:00 -0400

    feat: ssl config

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: '//localhost:9970/lp-api',
+  apiUrl: '//thelocaltiger.com:9970/lp-api',
   swWait: 1000,
   appVersion: 'local-test',
 };
